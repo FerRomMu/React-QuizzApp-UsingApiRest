971f8bf554dfe364677b43fcc0480bf16085e357
Fernando Romero <fer.rom.mu@gmail.com>
2022-11-20 22:59:50 -0300

    Cambios en QuizzContext para moverme entre containers.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import { useContext, useState } from 'react';
+import { useContext } from 'react';
 import Menu from './containers/menu/Menu';
 import Questions from './containers/questions/Questions';
 import Results from './containers/results/Results';
 import DifficultyContext from './context/DifficultyContext'
+import QuizzContext from './context/QuizzContext';
 
 function App() {
   const { difficulty } = useContext(DifficultyContext)
-  const hasEnded = false
-  console.log(difficulty)
+  const { quizz, endQuizz, setQuestions } = useContext(QuizzContext)
+  const hasEnded = quizz.hasEnded
+  console.log(quizz)
 
   return (
     <div>
       { !difficulty? <Menu/> : null }
       { difficulty && !hasEnded? <Questions/> : null }
       { difficulty && hasEnded? <Results/> : null }
+      <button onClick={ ()=> {
+        hasEnded? setQuestions([]) : endQuizz() } }>change</button>
     </div>
   );
 }
--- src/context/QuizzContext.jsx
+++ src/context/QuizzContext.jsx
@@ -4,10 +4,27 @@ const QuizzContext = createContext({})
 export default QuizzContext
 
 export const QuizzProvider = ({ children }) => {
-    const [quizz, setQuizzContext] = useState({});
+    const [quizz, setQuizzContext] = useState({
+        hasEnded: null,
+        questions: null
+    });
+
+    const setQuestions = (questions) => {
+        setQuizzContext({
+            hasEnded: false,
+            questions: questions
+        })
+    }
+
+    const endQuizz = () => {
+        setQuizzContext({
+            hasEnded: true,
+            questions: null
+        })
+    }
 
     return (
-        <QuizzContext.Provider value={{ quizz, setQuizzContext }}>
+        <QuizzContext.Provider value={{ quizz, setQuestions, endQuizz }}>
             {children}
         </QuizzContext.Provider>
     )
