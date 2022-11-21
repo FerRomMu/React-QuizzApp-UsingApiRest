d7dd12473ca86e7e72e76dc77fc602861fdfa47c
Fernando Romero <fer.rom.mu@gmail.com>
2022-11-20 23:32:51 -0300

    Boceto de container de questions.

--- src/containers/questions/Questions.jsx
+++ src/containers/questions/Questions.jsx
@@ -1,8 +1,32 @@
+import { useContext, useState } from "react"
+import OptionQuizz from "../../componentes/atoms/Option"
+import Subtitle from "../../componentes/atoms/Subtitle"
+import TextInfo from "../../componentes/atoms/TextInfo"
+import Title from "../../componentes/atoms/Title"
+import QuizzContext from "../../context/QuizzContext"
+
 const Questions = () => {
+    const preguntas = ["pregunta 1", "pregunta 2", "pregunta 3"]
+    const tempOp = ["opcion 1", "opcion 2"]
+    const [index, setIndex] = useState(0)
+    const { endQuizz } = useContext(QuizzContext)
+
+    const handleAnswer = () => {
+        if(index < preguntas.length-1){
+            setIndex(index+1)
+        } else { 
+            setIndex(0)
+            endQuizz()
+        }
+    }
+
     return (
-        <div>
-            Question
-        </div>
+        <>
+            <Title title="Preguntas"/>
+            <Subtitle subtitle={"Responde la pregunta (" + (index+1) + "/" + preguntas.length + ")"}/>
+            <TextInfo text={preguntas[index]}/>
+            { tempOp.map( op => <OptionQuizz option={op} action={ () => handleAnswer() }/>)}
+        </>
     )
 }
 
