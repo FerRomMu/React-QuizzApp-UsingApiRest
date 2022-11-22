beec8cb6287520461051b168fc1da05811157f0e
Fernando Romero <fer.rom.mu@gmail.com>
2022-11-21 21:41:36 -0300

    Se agrego obtención de preguntas de la api. Requiere refactor.

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import { useContext, useEffect } from "react";
+import DifficultyContext from "../context/DifficultyContext";
 
 const url = 'https://preguntados-api.vercel.app/api'
 
@@ -6,4 +8,11 @@ export const getDifficulty = (setData) => {
     axios.get(url + '/difficulty')
     .then( result => setData(result.data) )
     .catch( e => console.log("oops... an unexpected error has ocurred."))
+}
+
+export const getQuestionsByDifficulty = async (difficulty) => {
+    const result = await axios.get(url + '/questions?difficulty=' + difficulty)
+    .then( result => result.data )
+    .catch( e => console.log(e))
+    return result
 }
--- src/hooks/useQuizzActions.js
+++ src/hooks/useQuizzActions.js
@@ -1,25 +1,38 @@
-import { useContext, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { getQuestionsByDifficulty } from "../api/api"
+import DifficultyContext from "../context/DifficultyContext"
 import QuizzContext from "../context/QuizzContext"
 
 export const useQuizzActions = () => {
-    const getQuestions = ["pregunta 1", "pregunta 2", "pregunta 3"]
-    const getOptions = ["opcion 1", "opcion 2"]
+
+    const { difficulty } = useContext(DifficultyContext)
+    const [getQuestions, setQuestions] = useState([])
+    useEffect(() => {
+        async function wrap() {
+        const result = await getQuestionsByDifficulty(difficulty)
+        setQuestions(result);
+        console.log(result)
+        }
+        wrap()
+    }, [])
+    
     const [index, setIndex] = useState(0)
     const { endQuizz, pushAnswer } = useContext(QuizzContext)
     const checkAnswer = (op) => { return "opcion 1" === op }
-
+    
     const setAnswer = (op) => {
         pushAnswer(checkAnswer(op))
-        if(index < getQuestions.length-1){
+        if(index < getLength-1){
             setIndex(index+1)
         } else {
             setIndex(0)
             endQuizz()
         }
     }
-
+    
     const getLength = getQuestions.length
-    const getQuestion = getQuestions[index]
+    const getOptions = getQuestions[index]?.option1? [getQuestions[index].option1, getQuestions[index].option2, getQuestions[index].option3, getQuestions[index].option4] : []
+    const getQuestion = getQuestions[index]?.question? getQuestions[index].question : 'Cargando'
     
     return [getQuestion, index, getLength, getOptions, setAnswer]
 }
